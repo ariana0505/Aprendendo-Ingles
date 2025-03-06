64e68daeaa283a2a8ec8acd79f5568ae1e01cc25
ariana0505 <penaariana075@gmail.com>
2025-03-06 17:16:26 -0500

    avance

--- condicion.py
+++ condicion.py
@@ -1,10 +1,9 @@
 
-def condicional(palabraIngresada , valores):
-    score = 0
+def condicional(palabraIngresada , valores, score):
     if palabraIngresada in valores:
         print("Excelente! +1")
-        score =+ 1 
+        score += 1 
     else:
         valor = ", ".join(valores)
         print(f"Error el significado es {valor}")
-    
+    return score
--- main.py
+++ main.py
@@ -4,19 +4,22 @@
 
 
 from condicion import condicional
+from unidecode import unidecode
+
 #Inportaciones: 
 import random
 
 #Variables defecto
 diccionario = {}
 texto = []
+score =0
 
 #Abriendo fichero
 fichero = open('palabras.txt','r', encoding="utf-8")
 
 #Recorriendo fichero
 for linea in fichero:
-    texto.append(linea.strip())
+    texto.append(unidecode(linea.strip()))
 
 #Convirtiendo en diccionario
 for i in range(len(texto)):
@@ -25,16 +28,15 @@ for i in range(len(texto)):
     significado = valores[1:] 
     diccionario[palabra] = significado
 
+print(texto)
+intentos = int(input("Ingrese el numero de intentos: "))
 
-inte = int(input("Ingrese el numero de intentos"))
-
-for i in range(inte):
+for i in range(intentos):
     #obteniendo un item random
     clave, valor = random.choice(list(diccionario.items()))
     valor = ", ".join(valor)#me devuelve un str separado por comas
     valores = valor.split(", ")#me devuelveuna lista que esta separado por comas (", ")
-    palabraIngresada = input(f"Ingrese el significado de la la palabra {clave}: ")
-    condicional(palabraIngresada, valores)
-
-    
-    
+    palabraIngresada = input(f"Ingrese el significado de la la palabra {clave}: ").strip()
+    score = condicional(unidecode(palabraIngresada), valores, score)
+else:
+    print(f"Tu puntuacion es: {score}")
